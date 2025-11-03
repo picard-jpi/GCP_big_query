9e3019c8f0119cf04b2caefa8621b1e76de39f1d
picard-jpi <74557349+picard-jpi@users.noreply.github.com>
2025-11-03 10:32:01 +0100

    Add files via upload

---
+++ exemple_requete.py
@@ -0,0 +1,22 @@
+
+SELECT
+  name, gender,
+  SUM(number) AS total
+FROM
+  `bigquery-public-data.usa_names.usa_1910_2013`
+GROUP BY
+  name, gender
+ORDER BY
+  total DESC
+LIMIT
+  10
+
+####
+
+SELECT
+ name, count
+FROM
+ `babynames.names_2014`
+WHERE
+ gender = 'M'
+ORDER BY count DESC LIMIT 5
